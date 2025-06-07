5c181267e0c05d9c7b27dda61d791a36def6a1ac
Jose Silva <grillo.silgom@gmail.com>
2025-06-07 01:50:35 -0600

    Agregacion de fxml principalpilot y principalassistant

---
+++ src/javafxappaerolinea/controller/FXMLAssistantInfoController.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxappaerolinea.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author zenbook i5
+ */
+public class FXMLAssistantInfoController implements Initializable {
+
+    @FXML
+    private Label lbId;
+    @FXML
+    private Label lbName;
+    @FXML
+    private Label lbAddress;
+    @FXML
+    private Label lbBirthDate;
+    @FXML
+    private Label lbGender;
+    @FXML
+    private Label lbSalary;
+    @FXML
+    private Label lbEmail;
+    @FXML
+    private Label lbAssistanceHours;
+    @FXML
+    private Label lbNumberOfLanguages;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void btnEditProfile(ActionEvent event) {
+    }
+    
+}
---
+++ src/javafxappaerolinea/controller/FXMLPilotUpcomingFlightsController.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package javafxappaerolinea.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author zenbook i5
+ */
+public class FXMLPilotUpcomingFlightsController implements Initializable {
+
+    @FXML
+    private TextField tfFilterDestination;
+    @FXML
+    private TableView<?> tvUpcomingFlights;
+    @FXML
+    private TableColumn<?, ?> tcId;
+    @FXML
+    private TableColumn<?, ?> tcOrigin;
+    @FXML
+    private TableColumn<?, ?> tcDestination;
+    @FXML
+    private TableColumn<?, ?> tcDepartureDate;
+    @FXML
+    private TableColumn<?, ?> tcDepartureHour;
+    @FXML
+    private TableColumn<?, ?> tcArrivalDate;
+    @FXML
+    private TableColumn<?, ?> tcArrivalHour;
+    @FXML
+    private TableColumn<?, ?> tcGate;
+    @FXML
+    private TableColumn<?, ?> tcAirplane;
+    @FXML
+    private TableColumn<?, ?> tcAirline;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void btnFilter(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnClearFilter(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnViewDetails(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnExport(ActionEvent event) {
+    }
+    
+}
