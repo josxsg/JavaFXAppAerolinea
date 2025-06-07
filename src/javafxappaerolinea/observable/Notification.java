741b37a5bbb31fd52716965beb8f35a63bf62d28
Angel Puch <angeljonathanpuchhernandez@gmail.com>
2025-06-07 02:55:38 -0600

    Cambio de nombre notificacion

--- src/javafxappaerolinea/observable/Notification.java
+++ src/javafxappaerolinea/observable/Notification.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package javafxappaerolinea.interfaz;
+package javafxappaerolinea.observable;
 
 /**
  *
  * @author Dell
  */
-public interface Notificacion {
+public interface Notification {
     void operacionExitosa();
 }
